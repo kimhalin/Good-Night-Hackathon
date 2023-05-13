dcd71490e29658d49eff1e1471d7d20c842d33a2
HaLin Kim <halin130@tukorea.ac.kr>
2023-05-13 15:47:07 +0900

    chore: 엔티티 세팅

--- apps/infrastructure/infrastructure.module.ts
+++ apps/infrastructure/infrastructure.module.ts
@@ -8,6 +8,9 @@ import {
     patchTypeORMRepositoryWithBaseRepository
 } from "typeorm-transactional-cls-hooked";
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const entities = [
     Restaurant,
     Review
@@ -21,11 +24,10 @@ const entities = [
                 const config = {
                     host: process.env.DB_HOST,
                     port: Number(process.env.DB_PORT),
-                    username: process.env.DB_USERNAM,
+                    username: process.env.DB_USERNAME,
                     password: process.env.DB_PASSWORD,
                     database: process.env.DB_DATABASE
                 };
-
 
                 return {
                     type: 'mysql',
